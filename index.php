32d9c4171d468ab383604a2bd2d6fc89b78a1783
vitodursopettersson <vitodurso98@gmail.com>
2024-01-12 18:35:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi JSON</title>
+
+    <!-- Link Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+
+
+    <!-- Link Vue Js -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- Link Js -->
+    <script src="main.js"></script>
+
+</body>
+
+</html>
